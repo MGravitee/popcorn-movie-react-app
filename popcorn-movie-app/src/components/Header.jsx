2435b06ac155544a1d4f76847a37438a1562feb3
Luke Rushton <luke.rushtonx@gmail.com>
2024-07-29 12:44:30 -0700

    added minimal link structure in header

--- popcorn-movie-app/src/components/Header.jsx
+++ popcorn-movie-app/src/components/Header.jsx
@@ -1,6 +1,21 @@
+import { NavLink } from "react-router-dom";
+
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div>Header</div>
+      <ul>
+        <li>
+          <NavLink to="/">home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">about</NavLink>
+        </li>
+        <li>
+          <NavLink to="/favourites">favourites</NavLink>
+        </li>
+      </ul>
+    </header>
+  );
 }
-export default Header
+export default Header;
