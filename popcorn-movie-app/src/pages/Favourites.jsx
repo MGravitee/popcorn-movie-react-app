b58db09b73b48e6e26c1d80c494359d7fb15b26e
Ryan Oakman <ryanoakman@gmail.com>
2024-08-05 10:48:48 -0700

    Added code to show favourited movies on favourited page. Running into errors that need troubleshooting.

--- popcorn-movie-app/src/pages/Favourites.jsx
+++ popcorn-movie-app/src/pages/Favourites.jsx
@@ -1,6 +1,36 @@
+import {useContext} from "react";
+import {GlobalContext} from "../context/GlobalState";
+import {Link} from "react-router-dom";
+import Favourite from "../components/Favourite";
+
 function Favourites() {
+  const {favourites} = useContext(GlobalContext);
   return (
-    <div>Favourites</div>
-  )
+    <div>
+      <ul>
+        {favourites.length > 0 ? (
+          favourites.map((movie) => (
+            <li className="movie-card" key={movie.id}>
+              <Favourite movieData={movie} />
+              <div className="movie-title">{movie.title}</div>
+              <div className="movie-date">{movie.release_date}</div>
+              <div className="movie-poster">
+                <img
+                  src={`${baseImgEndPoint}${movie.poster_path}`}
+                  alt={movie.title}
+                />
+              </div>
+              <div className="movie-overview">{movie.overview}</div>
+              <button>
+                <Link to={`/detail/${movie.id}`}>More Info</Link>
+              </button>
+            </li>
+          ))
+        ) : (
+          <p>No favourites to display</p>
+        )}
+      </ul>
+    </div>
+  );
 }
-export default Favourites
+export default Favourites;
