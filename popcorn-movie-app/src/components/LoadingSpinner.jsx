284946c462221dde762051ff60bf62b4fecddcc3
MGravitee <mgravitee@gmail.com>
2024-08-06 08:35:34 -0700

    Added  images on about page, as well as text, have also started dealing with error page.

--- popcorn-movie-app/src/components/LoadingSpinner.jsx
+++ popcorn-movie-app/src/components/LoadingSpinner.jsx
@@ -1,6 +1,6 @@
 function LoadingSpinner() {
   return (
-    <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    <div className="lds-roller place-center"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
   )
 }
 export default LoadingSpinner
--- popcorn-movie-app/src/pages/About.jsx
+++ popcorn-movie-app/src/pages/About.jsx
@@ -1,9 +1,23 @@
 import { useState, useEffect } from "react";
-
+import { Link } from "react-router-dom";
 
 function About() {
   return (
-    <h1>About</h1>
+    <>
+
+        <img className="popcorn-logo" src="/public/popcorn-logo.svg" alt="Popcorn logo"/>
+        <p className="prgrph">At Popcorn, we are passionate about movies and strive to bring you the best movie experience possible. Our team of movie enthusiasts has developed a movie database app that is both comprehensive and user-friendly.</p>
+
+        <p className="prgrph">With Popcorn, you can discover new movies, stay up to date on the latest releases, and find the perfect flick to watch on any given day.</p>
+
+        <p className="prgrph">This product uses the TMDb API but is not endorsed or certified by TMDb. </p>
+    
+          <Link className="tmdb-link" to="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">
+            <img className="tmdb-logo" src="/public/tmdb-logo.svg" alt="TMDB Logo" />
+          </Link>
+    
+    </>
+    
 
   )
 }
