04a7b6fc8a25f489e785377b60b74ddb8f320faf
MGravitee <mgravitee@gmail.com>
2024-08-22 16:46:11 -0700

    added accessibility H1's to all pages that need them as well as moved styling from pretty well empty main.scss and into global

--- popcorn-movie-app/src/pages/Error.jsx
+++ popcorn-movie-app/src/pages/Error.jsx
@@ -1,15 +1,19 @@
+import {NavLink} from "react-router-dom";
+
+
 function Error() {
     return (
         <>
-
+            <h1 class="screen-reader-text">Popcorn Movies | Error Page</h1>
             <img className="caution-symbol" src="/caution-symbol.svg" alt="Caution symbol for error page"/> 
 
-            <h1>Uh oh.... something went wrong</h1>
+            <h2 className="prgrph">Uh oh.... something went wrong</h2>
 
-            <p>
+            <p className="prgrph">
                 Not sure what happened but we couldn't find the page you're
                 looking for.
             </p>
+            <NavLink to="/">Back to Home</NavLink>
         </>
     );
 }
