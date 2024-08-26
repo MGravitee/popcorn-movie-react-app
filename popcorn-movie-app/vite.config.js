86dcd5ae5d79d4442eb965adc4fa9e7052bab26c
Luke Rushton <luke.rushtonx@gmail.com>
2024-08-26 15:59:15 -0700

    setup for build

--- popcorn-movie-app/vite.config.js
+++ popcorn-movie-app/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/popcorn-movie/',
+  build: {
+    outDir: 'popcorn-movie'
+  },
   plugins: [react()],
 })
